f5875054b34a2cb36c3175e9c7f57867e0f105bf
Manoel Mathias <dev.mathiasx@gmail.com>
2023-05-06 14:49:42 -0300

    Some changes in the part of Log in and Register

--- web/src/components/Login/index.jsx
+++ web/src/components/Login/index.jsx
@@ -3,8 +3,8 @@ import Form from "./Form";
 
 export default function Login() {
     return (
-        <div className="flex flex-col mx-auto h-screen justify-between items-center">
-            <div className="flex flex-col mt-40 mx-auto xs0:px-8 items-center">
+        <div className="flex flex-col flex-1 mx-auto h-screen justify-between items-center">
+            <div className="flex flex-col mt-20 mx-auto xs0:px-8 items-center">
                 <div className="text-cyan-100 font-bold font-[roboto] text-xl mb-4">Faça o seu login!</div>
                 <Form/>
                 <div className="mt-2">
--- web/src/components/SignUp/Form.jsx
+++ web/src/components/SignUp/Form.jsx
@@ -30,6 +30,78 @@ export default function Form() {
                     type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirme sua senha"
                 />
 
+                <label htmlFor="birthday">Data de nascimento:</label>
+                <div className="flex gap-2">
+
+                    <select
+                        name="month"
+                        id="month"
+                        defaultValue={'default'}
+                        className="text-gray-600 outline-none font-medium bg-opacity-70 p-1 bg-cyan-100 rounded border-b-4 border-cyan-500"
+                    >
+                        <option value="default" disabled>Dia</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12">12</option>
+                    </select>
+                    <select
+                        name="day"
+                        id="day"
+                        defaultValue={'default'}
+                        className="text-gray-600 outline-none font-medium bg-opacity-70 p-1 bg-cyan-100 rounded border-b-4 border-cyan-500"
+                    >
+                        <option value="default" disabled>Mês</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12">12</option>
+                        <option value="13">13</option>
+                        <option value="14">14</option>
+                        <option value="15">15</option>
+                        <option value="16">16</option>
+                        <option value="17">17</option>
+                        <option value="18">18</option>
+                        <option value="19">19</option>
+                        <option value="20">20</option>
+                        <option value="21">21</option>
+                        <option value="22">22</option>
+                        <option value="23">23</option>
+                        <option value="24">24</option>
+                        <option value="25">25</option>
+                        <option value="26">26</option>
+                        <option value="27">27</option>
+                        <option value="28">28</option>
+                        <option value="29">29</option>
+                        <option value="30">30</option>
+                        <option value="31">31</option>
+                    </select>
+
+                    <input
+                        type="number"
+                        placeholder="Ano"
+                        aria-valuemin={1900}
+                        aria-valuemax={2020}
+                        className="w-16 text-gray-600 outline-none font-medium bg-opacity-70 p-1 bg-cyan-100 rounded border-b-4 border-cyan-500 placeholder:text-gray-600"
+                    />
+                </div>
+
                 <Button
                     width="w-full" height="h-10" padding="p-2" mt="mt-2"
                     text={"Cadastre-se"}
